07c7583bf3a75a4ca5435194ff3602539367bfe4
Edilson Luciano <edison591@gmail.com>
2024-07-18 12:41:36 -0300

    adicionando hero e a fonte

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,33 @@
-const gulp = require('gulp')
-const sass = require('gulp-sass')(require('sass'))
+const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const imagemin = require("gulp-imagemin");
+// const gulpIf = require("gulp-if");
 
 function styles() {
-    return gulp.src('./src/styles/*.scss')
-        .pipe(sass({outputStyle:'compressed'}))
-        .pipe(gulp.dest('./dist/css'));
+  return gulp
+    .src("./src/styles/*.scss")
+    .pipe(sass({ outputStyle: "compressed" }))
+    .pipe(gulp.dest("./dist/css"));
 }
 
-
-
-exports.default = styles;
-exports.watch = function() {
-    gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+function imagens() {
+  return gulp.src("./src/images/**/*")
+    .pipe(
+      imagemin([
+        imagemin.gifsicle({ interlaced: true }),
+        imagemin.mozjpeg({ quality: 75, progressive: true }),
+        imagemin.optipng({ optimizationLevel: 5 }),
+        imagemin.svgo({
+          plugins: [
+            { removeViewBox: false }, 
+            { cleanupIDs: false }],
+        }),
+      ])
+    )
+    .pipe(gulp.dest("./dist/imagens"));
 }
 
-
-
+exports.default = gulp.parallel(styles, imagens);
+exports.watch = function () {
+  gulp.watch("./src/styles/*.scss", gulp.parallel(styles));
+};
